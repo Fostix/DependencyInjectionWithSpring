886188920a424d407a41dd80c098bce21f3d597c
Albert <accretiondiskthat@gmail.com>
2023-03-08 11:57:06 +0500

    default Profiles annotation

--- src/main/java/com/springframework/spring6di/services/i18n/EnglishGreetingService.java
+++ src/main/java/com/springframework/spring6di/services/i18n/EnglishGreetingService.java
@@ -1,12 +1,10 @@
 package com.springframework.spring6di.services.i18n;
 
-import com.springframework.spring6di.controllers.i18n.Myi18NController;
 import com.springframework.spring6di.services.GreetingService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+@Profile({"EN", "default"})
 @Service("i18NService")
 public class EnglishGreetingService implements GreetingService {
     @Override
