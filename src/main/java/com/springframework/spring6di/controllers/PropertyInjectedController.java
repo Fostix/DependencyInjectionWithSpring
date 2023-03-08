df1f54efae9d66c59f937a0d68d507988edba69f
Albert <accretiondiskthat@gmail.com>
2023-03-08 08:42:32 +0500

    refactor used spring annotations

---
+++ src/main/java/com/springframework/spring6di/controllers/PropertyInjectedController.java
@@ -0,0 +1,16 @@
+package com.springframework.spring6di.controllers;
+
+import com.springframework.spring6di.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PropertyInjectedController {
+
+    @Autowired
+    GreetingService greetingService;
+
+    public String sayHello() {
+        return greetingService.sayGreeting();
+    }
+}
